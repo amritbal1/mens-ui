54cae65d88b7ce0f641b9aae17701ed966244ffc
Amrit Bal <amrit.bal@hotmail.co.uk>
2022-07-12 22:32:33 +0100

    Styling

--- src/components/ProductFinder/utils/optionsCreator.js
+++ src/components/ProductFinder/utils/optionsCreator.js
@@ -9,7 +9,7 @@ export const getOption = ({
 }) => {
   const selectedOptionStyles = isChecked ? "border border-gray-400" : "";
   const hoverDivStyles = `${selectedOptionStyles} sm:mx-1 md:hover:border md:hover:border-2 rounded-3xl w-110px sm:h-140px sm:w-140px md:h-160px md:w-160px flex flex-grow flex-col justify-center items-center cursor-pointer md:hover:bg-gray-100 p-2`;
-  const outerDivStyle = `rounded-full h-110px sm:h-140px md:h-160px mb-8 mx-1`;
+  const outerDivStyle = `rounded-full h-110px sm:h-140px md:h-160px mb-8 mx-0.5 sm:mx-1`;
   return (
     <div class={outerDivStyle} key={value}>
       <div class={hoverDivStyles} onClick={() => clickHandlerFn(value)}>
