74bed0f24420cd7aa86155f46d69c974b2b7c3d1
Amrit Bal <amrit.bal@hotmail.co.uk>
2022-10-23 16:05:04 +0100

    Remove unused dependencies

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,11 @@
 import React, { PureComponent, createRef } from "react";
-import SearchBar from "../../components/SearchBar/SearchBar";
 import { withRouter } from "react-router-dom";
 import { withAuth0 } from "@auth0/auth0-react";
-// import ProfileIcon from "../ProfileIcon/ProfileIcon";
 import AppContext from "../../AppContext";
-// import { MenuIcon, XIcon } from "@heroicons/react/outline";
-// import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { LOCAL_STORAGE_ITEM, SORTING_FIELD } from "../../utils/enums";
 import * as logo from "../../images/brand-logo.png";
 
-// const navbarItemStyle =
-//   "cursor-pointer lg:ml-10 text-slate-gray uppercase hover:opacity-70 rounded-full px-3 py-4 text-xs tracking-wider";
-// const menuItemStyle =
-//   "text-lg uppercase font-light text-slate-gray block rounded-md cursor-pointer cursor-pointer border-b py-4";
 class Navbar extends PureComponent {
   state = {
     isPanelOpen: false,
@@ -106,7 +98,6 @@ class Navbar extends PureComponent {
   };
 
   render() {
-    const { displaySearchBar = false } = this.props;
     // const { isAuthenticated } = auth0;
     const { isPanelOpen } = this.state;
     return (
@@ -130,86 +121,19 @@ class Navbar extends PureComponent {
                     </div>
 
                     <div className="hidden md:block">
-                      <div className="ml-4 flex items-baseline space-x-2 justify-between">
-                        {/* <Fragment>
-                          <span
-                            class="text-slate-gray cursor-pointer mr-4"
-                            onClick={this.handleLogoClick}
-                          >
-                            Product Finder
-                          </span>
-                        </Fragment> */}
-                      </div>
+                      <div className="ml-4 flex items-baseline space-x-2 justify-between"></div>
                     </div>
                   </div>
-                  {displaySearchBar && (
-                    <div className="md:hidden w-full">
-                      <SearchBar
-                        overrideStyles={{
-                          menuWidth: "80vw",
-                          position: "absolute",
-                          marginLeft: "-50px",
-                        }}
-                      />
-                    </div>
-                  )}
-                  {displaySearchBar && (
-                    <div className="hidden md:block w-full max-w-4xl">
-                      <SearchBar />
-                    </div>
-                  )}
+
                   <div className="hidden md:block flex-shrink-0">
                     <div className="flex items-center">
                       {/* Profile dropdown */}
                       <Menu as="div" className="relative">
-                        <div className="flex items-center">
-                          {/* <Fragment>
-                            <div
-                              className={navbarItemStyle}
-                              onClick={this.handleViewAllProducts}
-                            >
-                              All Products
-                            </div>
-                          </Fragment> */}
-                          {/* <Fragment>
-                            <div
-                              className={navbarItemStyle}
-                              onClick={this.handleWriteReviewClick}
-                            >
-                              Write A Review
-                            </div>
-                          </Fragment> */}
-                          {/* <Fragment>
-                            {!isAuthenticated && (
-                              <div
-                                className={navbarItemStyle}
-                                onClick={this.handleUserLogin}
-                              >
-                                Sign Up
-                              </div>
-                            )}
-                            {isAuthenticated && <ProfileIcon />}
-                          </Fragment> */}
-                        </div>
+                        <div className="flex items-center"></div>
                       </Menu>
                     </div>
                   </div>
-                  <div className="-mr-2 flex md:hidden">
-                    {/* Mobile menu button */}
-                    {/* <div onClick={this.handleMenuButtonClick}>
-                      <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-lilac-700 md:hover:text-white md:hover:opacity-70 focus:outline-none ">
-                        <span className="sr-only">Open main menu</span>
-                        {isPanelOpen ? (
-                          <XIcon className="block h-6 w-6" aria-hidden="true" />
-                        ) : (
-                          <MenuIcon
-                            className="block h-6 w-6"
-                            aria-hidden="true"
-                          />
-                        )}
-                      </Disclosure.Button>
-                    </div> */}
-                  </div>
+                  <div className="-mr-2 flex md:hidden"></div>
                 </div>
               </div>
               <Transition
@@ -222,65 +146,7 @@ class Navbar extends PureComponent {
                 show={isPanelOpen}
               >
                 <Disclosure.Panel static className="md:hidden">
-                  <div className="fixed right-0 bg-white h-screen w-300px overflow-x-hidden z-10 px-4 pt-5 pb-5 shadow-md">
-                    {/* <Fragment>
-                      <div
-                        className={menuItemStyle}
-                        onClick={this.handleLogoClick}
-                      >
-                        Product Finder
-                      </div>
-                    </Fragment> */}
-                    {/* <Fragment>
-                      <div
-                        className={menuItemStyle}
-                        onClick={this.handleViewAllProducts}
-                      >
-                        All Products
-                      </div>
-                    </Fragment> */}
-                    {/* <Fragment>
-                      <div
-                        className={menuItemStyle}
-                        onClick={this.handleWriteReviewClick}
-                      >
-                        Write A Review
-                      </div>
-                    </Fragment> */}
-                    {/* If authenticated, show profile settings and log out or else show log in */}
-                    {/* {isAuthenticated && (
-                      <div>
-                        <Fragment>
-                          <div
-                            className={menuItemStyle}
-                            onClick={this.handleProfileSettingsClick}
-                          >
-                            Profile Settings
-                          </div>
-                        </Fragment>
-                        <Fragment>
-                          <div
-                            className={menuItemStyle}
-                            onClick={this.handleLogout}
-                          >
-                            Logout
-                          </div>
-                        </Fragment>
-                      </div>
-                    )} */}
-                    {/* {!isAuthenticated && (
-                      <div>
-                        <Fragment>
-                          <div
-                            className={menuItemStyle}
-                            onClick={this.handleUserLogin}
-                          >
-                            Sign in
-                          </div>
-                        </Fragment>
-                      </div>
-                    )} */}
-                  </div>
+                  <div className="fixed right-0 bg-white h-screen w-300px overflow-x-hidden z-10 px-4 pt-5 pb-5 shadow-md"></div>
                 </Disclosure.Panel>
               </Transition>
             </>
