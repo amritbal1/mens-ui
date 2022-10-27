a876ae5ec0ab233761db02c5da075b38b9f0fc15
Amrit Bal <amrit.bal@hotmail.co.uk>
2022-10-27 15:42:33 +0100

    Temp remove category filter

--- src/components/FilterPanel/utils/filterPillUtils/getFilterPillsConfig.js
+++ src/components/FilterPanel/utils/filterPillUtils/getFilterPillsConfig.js
@@ -1,27 +1,24 @@
-import { config } from "../../../../utils/config.js";
+// import { config } from "../../../../utils/config.js";
 import { FILTER_PILL_NAME } from "../../../../utils/enums.js";
 
 export const getFilterPillsConfig = ({ filterOptionClickFn }) => {
-  const { productCategoryOptions: productCategories } = config;
+  // const { productCategoryOptions: productCategories } = config;
 
-  const productCategoriesPill = {
-    label: FILTER_PILL_NAME.PRODUCT_CATEGORIES,
-    data: productCategories,
-    filterField: "filterProductCategoryValues",
-    allValuesField: "productCategoryValues",
-    urlParam: "productCategories",
-    filterOptionClickFn: filterOptionClickFn,
-    isSingleSelect: false,
-  };
+  // const productCategoriesPill = {
+  //   label: FILTER_PILL_NAME.PRODUCT_CATEGORIES,
+  //   data: productCategories,
+  //   filterField: "filterProductCategoryValues",
+  //   allValuesField: "productCategoryValues",
+  //   urlParam: "productCategories",
+  //   filterOptionClickFn: filterOptionClickFn,
+  //   isSingleSelect: false,
+  // };
 
-   const pricePill = {
+  const pricePill = {
     label: FILTER_PILL_NAME.PRICE,
     filterField: "filterMinPrice",
     filterOptionClickFn: filterOptionClickFn,
   };
 
-  return [
-    productCategoriesPill,
-    pricePill
-  ];
+  return [pricePill];
 };
