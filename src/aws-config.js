70e70812e9400aea309e9799bba5fbc77389054b
amritbal1 <72273976+amritbal1@users.noreply.github.com>
2022-11-29 14:48:58 +0000

    Delete aws-config.js

--- src/aws-config.js
+++
@@ -1,6 +0,0 @@
-export const S3_BUCKET = `${process.env.REACT_APP_S3_MEDIA_BUCKET}`;
-export const REGION = "eu-west-1";
-export const ACCESS_KEY = "AKIAQJEPYN2IV7RY6HHH";
-export const SECRET_ACCESS_KEY = "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";
-export const IDENTITY_POOL_ID =
-  "eu-west-1:95f55649-e558-493f-9352-de881daeec92";
